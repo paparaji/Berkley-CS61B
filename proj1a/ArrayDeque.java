cfa8af4ac44419dad56ac8b9c65411665994eadf
dingqy <41354044+dingqy@users.noreply.github.com>
2019-04-22 12:23:56 +0800

    Project 1a has been finished

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,92 +1,110 @@
 public class ArrayDeque<T> {
-    private T item[];
+    private T[] item;
     private int size;
 
-    public ArrayDeque(){
+    public ArrayDeque() {
         item = (T[]) new Object[8];
         size = 0;
     }
 
-    public ArrayDeque(ArrayDeque other){
+    public ArrayDeque(ArrayDeque other) {
         item = (T[]) new Object[other.item.length];
         System.arraycopy(item, 0, other.item, 0, other.size());
     }
 
-    /** Resize the item */
-    private void resize(int capacity){
-        T temp[] = (T[]) new Object[capacity];
+    /**
+     * Resize the item
+     */
+    private void resize(int capacity) {
+        T[] temp = (T[]) new Object[capacity];
         System.arraycopy(item, 0, temp, 0, size);
         item = temp;
     }
 
-    /** Add the item to the first of the AList */
-    public void addFirst(T add){
-        if (item.length < size + 1){
+    /**
+     * Add the item to the first of the AList
+     */
+    public void addFirst(T add) {
+        if (item.length < size + 1) {
             resize(size * 2);
         }
-        T temp[] = (T[]) new Object[item.length];
+        T[] temp = (T[]) new Object[item.length];
         System.arraycopy(item, 0, temp, 1, size);
         temp[0] = add;
         size += 1;
     }
 
-    /** Add the item to the last of the ALisr */
-    public void addLast(T add){
-        if(item.length < size + 1){
+    /**
+     * Add the item to the last of the AList
+     */
+    public void addLast(T add) {
+        if (item.length < size + 1) {
             resize(size * 2);
         }
         item[size] = add;
         size += 1;
     }
 
-    /** Determine whether the AList is empty */
-    public boolean isEmpty(){
+    /**
+     * Determine whether the AList is empty
+     */
+    public boolean isEmpty() {
         return size == 0;
     }
 
-    /** Return the size of the AList */
-    public int size(){
+    /**
+     * Return the size of the AList
+     */
+    public int size() {
         return size;
     }
 
-    /** Print the AList */
-    public void printDeque(){
-        for(int i = 0;i < size; i++){
+    /**
+     * Print the AList
+     */
+    public void printDeque() {
+        for (int i = 0; i < size; i++) {
             System.out.print(item[i] + " ");
         }
         System.out.println();
     }
 
-    /** Remove the first item of the AList */
-    public T removeFirst(){
-        if(0.25 * item.length > size && item.length >= 16){
-            resize((int)0.25 * item.length);
+    /**
+     * Remove the first item of the AList
+     */
+    public T removeFirst() {
+        if (0.25 * item.length > size && item.length >= 16) {
+            resize((int) 0.25 * item.length);
         }
 
-        T temp[] = (T[]) new Object[item.length];
+        T[] temp = (T[]) new Object[item.length];
         System.arraycopy(item, 1, temp, 0, size - 1);
-        T remove_item = item[0];
+        T remove = item[0];
         item = temp;
         size -= 1;
-        return remove_item;
+        return remove;
     }
 
-    /** Remove the last item of the AList */
-    public T removeLast(){
-        if(0.25 * item .length > size && item.length >= 16){
-            resize((int) 0.25*item.length);
+    /**
+     * Remove the last item of the AList
+     */
+    public T removeLast() {
+        if (0.25 * item.length > size && item.length >= 16) {
+            resize((int) 0.25 * item.length);
         }
-        T temp[] = (T[]) new Object[item.length];
+        T[] temp = (T[]) new Object[item.length];
         System.arraycopy(item, 0, temp, 0, size - 1);
-        T remove_item = item[size - 1];
+        T remove = item[size - 1];
         item = temp;
         size -= 1;
-        return remove_item;
+        return remove;
     }
 
-    /** Return the ith item of the AList */
-    public T get(int index){
-        if(index > size - 1){
+    /**
+     * Return the ith item of the AList
+     */
+    public T get(int index) {
+        if (index > size - 1) {
             return null;
         }
         return item[index];
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,10 +1,10 @@
 public class LinkedListDeque<T> {
-    private class LinkedList<T2>{
+    private class LinkedList<T2> {
         private T2 item;
         private LinkedList next;
         private LinkedList prev;
 
-        public LinkedList(T2 item, LinkedList next, LinkedList prev){
+        LinkedList(T2 item, LinkedList next, LinkedList prev) {
             this.item = item;
             this.next = next;
             this.prev = prev;
@@ -13,9 +13,9 @@ public class LinkedListDeque<T> {
 
     private LinkedList<Integer> sentinel = new LinkedList<Integer>(-51723, null, null);
     private int size = 0;
-    private LinkedList list_recursion = sentinel;
+    private LinkedList recursion = sentinel;
 
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
     }
@@ -23,54 +23,66 @@ public class LinkedListDeque<T> {
     /**
      * @source https://www.youtube.com/watch?v=JNroRiEG7U4
      */
-    public LinkedListDeque(LinkedListDeque other){
+    public LinkedListDeque(LinkedListDeque other) {
         sentinel.next = sentinel;
         sentinel.prev = sentinel;
 
-        for(int i = 0;i < other.size();i++){
+        for (int i = 0; i < other.size(); i++) {
             addLast((T) other.get(i));
         }
     }
 
-    /** Add the item to the first of the list */
-    public void addFirst(T item){
+    /**
+     * Add the item to the first of the list
+     */
+    public void addFirst(T item) {
         LinkedList<T> temp = new LinkedList<>(item, sentinel.next, sentinel);
         sentinel.next.prev = temp;
         sentinel.next = temp;
         size += 1;
     }
 
-    /** Add the item to the end of the list */
-    public void addLast(T item){
+    /**
+     * Add the item to the end of the list
+     */
+    public void addLast(T item) {
         LinkedList<T> temp = new LinkedList<>(item, sentinel, sentinel.prev);
         sentinel.prev.next = temp;
         sentinel.prev = temp;
         size += 1;
     }
 
-    /** Determine whether the list is empty */
-    public boolean isEmpty(){
+    /**
+     * Determine whether the list is empty
+     */
+    public boolean isEmpty() {
         return sentinel.next == sentinel;
     }
 
-    /** Return the size of the list */
-    public int size(){
+    /**
+     * Return the size of the list
+     */
+    public int size() {
         return size;
     }
 
-    /** Print out all the list */
-    public void printDeque(){
+    /**
+     * Print out all the list
+     */
+    public void printDeque() {
         LinkedList temp = sentinel.next;
-        while (temp != sentinel){
+        while (temp != sentinel) {
             System.out.print(temp.item + " ");
             temp = temp.next;
         }
         System.out.println();
     }
 
-    /** Remove the first item of the list */
-    public T removeFirst(){
-        if(this.isEmpty()){
+    /**
+     * Remove the first item of the list
+     */
+    public T removeFirst() {
+        if (this.isEmpty()) {
             return null;
         }
         T temp = (T) sentinel.next.item;
@@ -79,9 +91,11 @@ public class LinkedListDeque<T> {
         return temp;
     }
 
-    /** Remove the last item of the list */
-    public T removeLast(){
-        if(this.isEmpty()){
+    /**
+     * Remove the last item of the list
+     */
+    public T removeLast() {
+        if (this.isEmpty()) {
             return null;
         }
         T temp = (T) sentinel.prev.item;
@@ -90,31 +104,35 @@ public class LinkedListDeque<T> {
         return temp;
     }
 
-    /** Return the ith item of the list */
-    public T get(int index){
-        list_recursion = list_recursion.next;
-        if(index == 0 && list_recursion != sentinel){
-            return (T) list_recursion.item;
-        }else if(index != 0 && list_recursion == sentinel){
+    /**
+     * Return the ith item of the list
+     */
+    public T get(int index) {
+        recursion = recursion.next;
+        if (index == 0 && recursion != sentinel) {
+            return (T) recursion.item;
+        } else if (index != 0 && recursion == sentinel) {
             return null;
-        }else{
+        } else {
             return get(index - 1);
         }
     }
 
-    /** Return the ith item of the list by iteration */
-    public T getRecursive(int index){
-        if(this.isEmpty()){
+    /**
+     * Return the ith item of the list by iteration
+     */
+    public T getRecursive(int index) {
+        if (this.isEmpty()) {
             return null;
         }
         LinkedList<T> temp = sentinel.next;
-        while (temp != sentinel && index > 0){
+        while (temp != sentinel && index > 0) {
             temp = temp.next;
             index -= 1;
         }
-        if(temp == sentinel){
+        if (temp == sentinel) {
             return null;
-        }else{
+        } else {
             return temp.item;
         }
     }
