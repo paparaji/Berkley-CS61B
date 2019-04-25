59e74e7368212e85288918ce8a3055ad1566a558
dingqy <41354044+dingqy@users.noreply.github.com>
2019-04-25 12:45:27 +0800

    Project 1gold has been finished

---
+++ proj1gold/TestArrayDequeGold.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+
+    @Test
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> test1 = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> test2 = new ArrayDequeSolution<>();
+        Integer studentOutput;
+        Integer correctOutput;
+        while (true) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            int number = StdRandom.uniform(10);
+            if (numberBetweenZeroAndOne > 0.75) {
+                test1.addFirst(number);
+                test2.addFirst(number);
+                System.out.printf("addFirst(%d)\n", number);
+            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
+                test1.addLast(number);
+                test2.addLast(number);
+                System.out.printf("addLast(%d)\n", number);
+            } else if (numberBetweenZeroAndOne < 0.5 && numberBetweenZeroAndOne >= 0.25
+                    && !test1.isEmpty() && !test2.isEmpty()) {
+                studentOutput = test1.removeFirst();
+                correctOutput = test2.removeFirst();
+                System.out.printf("removeFirst()\n");
+                assertEquals("\nStudent was " + studentOutput + ", Correct was " + correctOutput
+                        + " .", correctOutput, studentOutput);
+            } else if (numberBetweenZeroAndOne >= 0 && numberBetweenZeroAndOne < 0.25
+                    && !test1.isEmpty() && !test2.isEmpty()) {
+                studentOutput = test1.removeLast();
+                correctOutput = test2.removeLast();
+                System.out.printf("removeLast()\n");
+                assertEquals("\nStudent was " + studentOutput + ", Correct was " + correctOutput
+                        + ".", correctOutput, studentOutput);
+            }
+        }
+    }
+}
