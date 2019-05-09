cd49cb2c068ce31fe45dd01b17a029b819a09f37
dingqy <41354044+dingqy@users.noreply.github.com>
2019-05-09 21:47:21 +0800

    Clab 6 has been finished

--- clab/clab6/BubbleGrid.java
+++ clab/clab6/BubbleGrid.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class BubbleGrid {
     private int[][] grid;
 
@@ -14,6 +16,100 @@ public class BubbleGrid {
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
         // TODO
-        return null;
+        int count = 0;
+        int row = grid.length;
+        int column = grid[0].length;
+        int[] result = new int[darts.length];
+        boolean exist = false;
+        boolean exist_bubble = false;
+        for (int[] r : grid) {
+            for (int c : r) {
+                if (c == 1) {
+                    count++;
+                }
+            }
+        }
+        int temp = count;
+        UnionFind Bubbles = new UnionFind(row * column);
+        for (int i = 0; i < darts.length; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (grid[darts[i][0]][darts[i][1]] == 0) {
+                    result[i] = 0;
+                } else {
+                    grid[darts[i][0]][darts[i][1]] = 0;
+                    for (int k = 1; k < row; k++) {
+                        for (int x = 0; x < column; x++) {
+                            if (grid[k][x] == 1) {
+                                if (grid[k - 1][x] == 1) {
+                                    for (int y = 0; y < column; y++) {
+                                        if (Bubbles.connected(grid[0][y], (k - 1) * column + x)) {
+                                            Bubbles.union(k * column + x, (k - 1) * column + x);
+                                            exist_bubble = true;
+                                        }
+                                    }
+                                }
+                                if (x != 0 && grid[k][x - 1] == 1) {
+                                    for (int y = 0; y < column; y++) {
+                                        if (Bubbles.connected(grid[0][y], k * column + x - 1)) {
+                                            Bubbles.union(k * column + x, k * column + x - 1);
+                                            exist_bubble = true;
+                                        }
+                                    }
+
+                                }
+                                if (x != column - 1 && grid[k][x + 1] == 1) {
+                                    for (int y = 0; y < column; y++) {
+                                        if (Bubbles.connected(grid[0][y], k * column + x + 1)) {
+                                            Bubbles.union(k * column + x, k * column + x + 1);
+                                            exist_bubble = true;
+                                        }
+                                    }
+                                }
+                            }
+                            if (exist_bubble) {
+                                exist = true;
+                            } else {
+                                grid[k][x] = 0;
+                            }
+                        }
+                        if (!exist) {
+                            break;
+                        }
+                    }
+                    grid[darts[i][0]][darts[i][1]] = 1;
+                    if (grid[darts[i][0]][darts[i][1]] == 1) {
+                        for (int y = 0; y < column; y++) {
+                            if (Bubbles.connected(grid[0][y], (darts[i][0] - 1) * column + darts[i][1])) {
+                                Bubbles.union(darts[i][0] * column + darts[i][1], (darts[i][0] - 1) * column + darts[i][1]);
+                            }
+                        }
+                    }
+                    if (darts[i][1] != 0 && grid[darts[i][0]][darts[i][1] - 1] == 1) {
+                        for (int y = 0; y < column; y++) {
+                            if (Bubbles.connected(grid[0][y], darts[i][0] * column + darts[i][1] - 1)) {
+                                Bubbles.union(darts[i][0] * column + darts[i][1], darts[i][0] * column + darts[i][1] - 1);
+                            }
+                        }
+
+                    }
+                    if (darts[i][1] != column - 1 && grid[darts[i][0]][darts[i][1] + 1] == 1) {
+                        for (int y = 0; y < column; y++) {
+                            if (Bubbles.connected(grid[0][y], darts[i][0] * column + darts[i][1] + 1)) {
+                                Bubbles.union(darts[i][0] * column + darts[i][1], darts[i][0] * column + darts[i][1] + 1);
+                            }
+                        }
+                    }
+                }
+            }
+            for (int k = 0; k < column; k++) {
+                if (grid[0][k] == 1) {
+                    temp = temp - Bubbles.sizeOf(k);
+                }
+            }
+            result[i] = temp;
+            exist = false;
+            Bubbles = new UnionFind(row * column);
+        }
+        return result;
     }
 }
